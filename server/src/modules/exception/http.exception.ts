31a0a96ef2c6afb775786203014efe92d12ec257
Eliezer Souza <vnny77@hotmail.com>
2019-04-09 22:07:24 -0300

    fix: Correction at the HttpException

--- server/src/modules/exception/http.exception.ts
+++ server/src/modules/exception/http.exception.ts
@@ -1,18 +1,9 @@
-// class HttpException extends Error {
-//   status: number;
-//   message: string;
-
-//   constructor(status: number, message: string) {
-//     super(message);
-//     this.status = status;
-//     this.message = message;
-//   }
-// }
-class HttpException {
+class HttpException extends Error {
   status: number;
   message: string;
 
   constructor(status: number, message: string) {
+    super(message);
     this.status = status;
     this.message = message;
   }
